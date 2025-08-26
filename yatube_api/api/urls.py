e24b0df8272892c98b313cabb43310b6c59b1da8
Елена Цепова <tsepova.lena@gmail.com>
2025-08-26 21:09:56 +0300

    Приведение к PEP8

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -13,13 +13,24 @@ router.register('groups', GroupViewSet, basename='groups')
 router.register('follow', FollowViewSet, basename='follow')
 
 urlpatterns = [
-    path('jwt/create/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('jwt/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('jwt/create/', TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('jwt/refresh/', TokenRefreshView.as_view(),
+         name='token_refresh'),
+    path('jwt/verify/', TokenVerifyView.as_view(),
+         name='token_verify'),
 
     path('', include(router.urls)),
 
     # Вложенные ручные маршруты комментариев к постам
-    path('posts/<int:post_id>/comments/', CommentViewSet.as_view({'get': 'list', 'post': 'create'}), name='post-comments-list'),
-    path('posts/<int:post_id>/comments/<int:pk>/', CommentViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}), name='post-comments-detail'),
+    path('posts/<int:post_id>/comments/',
+         CommentViewSet.as_view({'get': 'list',
+                                 'post': 'create'}),
+         name='post-comments-list'),
+    path('posts/<int:post_id>/comments/<int:pk>/',
+         CommentViewSet.as_view({'get': 'retrieve',
+                                 'put': 'update',
+                                 'patch': 'partial_update',
+                                 'delete': 'destroy'}),
+        name='post-comments-detail'),
 ]
